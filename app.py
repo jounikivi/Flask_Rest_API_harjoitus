d81c7b8fb1b8130855b6d9bf94d112c1715bf769
Jouni Kiviperä <jounikivipera@gmail.com>
2022-06-01 10:43:19 +0300

    aloitus

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
-if __name__ == '__main__':
-    app.run(debug=True)
+    return '<h1>Hello World</h1>'
